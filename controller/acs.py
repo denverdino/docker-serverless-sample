c3f0ff698537def77d940726c6525fd871a54f16
Li Yi <denverdino@gmail.com>
2016-08-28 21:58:01 +0800

    Add the acs client
    Change-Id: I6d71eb4169116dbbd4658eac484aca644144e6f7

---
+++ controller/acs.py
@@ -0,0 +1,33 @@
+import docker
+
+class AliyunContainerServiceClient(docker.Client):
+    def __init__(self, *args, **kwargs):
+        super(AliyunContainerServiceClient, self).__init__(*args, **kwargs)
+
+    def projects(self, q=None, services=True, containers=True):
+        u = self._url('/projects/')
+        params = {
+        	'q': q,
+        	'services': services,
+            'containers': containers
+        }
+        res = self._result(self._get(u, params=params), True)
+        return res
+
+    def inspect_project(self, project):
+        return self._result(
+            self._get(self._url("/projects/{0}", project)), True
+        )
+
+    def create_project(self, name, description='', version='1.0', template='', environment=None):
+    	data = {
+            'name': name,
+            'description': description,
+            'version': version,
+            'template': template,
+            'environment': environment
+        }
+        u = self._url('/projects/')
+        res = self._post_json(u, data=data)
+        return res
+
--- controller/app.py
+++ controller/app.py
@@ -1,28 +1,54 @@
 from flask import Flask
-from redis import Redis
 import os
 import docker
+import time
+import acs
 
 app = Flask(__name__)
-#redis = Redis(host='redis', port=6379)
 docker_client = None
+acs_client = None
 
-def get_docker_client():  
+def init_clients():  
     tls_config = docker.tls.TLSConfig(
         client_cert=('/etc/docker/service.pem', '/etc/docker/service-key.pem'),
         verify='/etc/docker/acs-ca.pem'
     )
     cluster_url = os.environ.get('CLUSTER_URL')
-    return docker.Client(base_url=cluster_url, tls=tls_config)
+    docker_client = docker.Client(base_url=cluster_url, tls=tls_config)
+    acs_client = acs.AliyunContainerServiceClient(base_url=cluster_url, tls=tls_config)
 
-@app.route('/')
-def hello():
-    #redis.incr('hits')
+
+@app.route('/test1')
+def hello_from_docker():
     container = docker_client.create_container('busybox')
     docker_client.start(container)
     return 'Hello World from container %s!' % container
 
 
-if __name__ == "__main__":
-    docker_client = get_docker_client()
+test_template = '''
+version: "2"
+labels:
+  aliyun.project_type: "batch"
+services:
+  test:
+    image: registry.cn-hangzhou.aliyuncs.com/denverdino/docker-serverless-sample
+    restart: no
+    cpu_shares:  10
+    mem_limit: 100000000
+    labels:
+      aliyun.addon: "serverless"
+      aliyun.scale: "10"
+      aliyun.retry_count: "20"
+      aliyun.remove_containers: "remove-all"
+'''
+
+@app.route('/test2')
+def hello_from_aliyun():
+    project_name="hello%d" % time.time()
+    acs_client.create_project(project_name, template=test_template)
+    return 'Hello World from Aliyun %s!' % project_name
+
+
+if __name__ == '__main__':
+    init_clients()
     app.run(host="0.0.0.0", debug=True)
